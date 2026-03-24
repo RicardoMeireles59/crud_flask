21909ec941f4bb24c00fca98d4fb671adf91dd96
Ricardo Meireles <0000501220@senaimgaluno.com.br>
2026-03-24 20:12:33 -0300

    Ajuste para que as rotas funcionem adequadamente

--- app.py
+++ app.py
@@ -24,63 +24,69 @@ def executar_query(query, *args, fetch=False, commit=False):
 """ GET (Listar Todos): Retornar uma lista JSON de todos os registros.GET (Buscar por ID): Retornar apenas um registro específico ou erro 404. """
 
 @app.route('/jogos', methods=['GET'])
+@app.route('/jogos/<int:id>', methods=['GET'])
 
-def buscar_jogo():
-    id_jogo = 0 
-    jogo = executar_query("SELECT * FROM jogos WHERE id = ? ", id_jogo, fetch=True)
-    if jogo: 
-        return jsonify(dict(jogos[0])), 200 
-    return jsonify({"erro": "Jogo não encontrado"}), 404
+def buscar_jogo(id=None):
+    if id:
+        jogo = executar_query("SELECT * FROM jogos WHERE id = ? ", id, fetch=True)
+        if jogo: 
+            return jsonify(dict(jogo[0])), 200 
+        return jsonify({"erro": "Jogo não encontrado"}), 404
+
+    jogo = executar_query("SELECT id, jogo, autor, ano, preco, data_cadastro FROM jogos", fetch=True)
+    lista_jogos = [dict(item) for item in jogo]
+    return jsonify(lista_jogos), 200
 
 """POST (Inserir): Receber um JSON e salvar no banco (com status 201).""" 
  
 @app.route('/adicionarjogo', methods=['POST'])
 
 def adicionar_jogo():
-    jogos = ("Minecraft", "Mojang Studios", 2011, 89.90)
-
     jogos = request.get_json()
 
     executar_query(
         "INSERT INTO jogos (jogo, autor, ano, preco ) VALUES (?, ?, ?, ?)",
-        (
+        
             jogos.get('jogo'), 
             jogos.get('autor'), 
             jogos.get('ano'), 
-            jogos.get('preco')
-        ), commit=True
+            jogos.get('preco'), 
+            commit=True
     )
     return jsonify({"mensagem": "Jogo adicionado com sucesso!"}), 201
 
 """PUT (Atualizar): Alterar dados de um registro existente via ID (status 204). """
 
-@app.route('/atualizarjogo', methods=['PUT'])
-
+@app.route('/jogos/<int:id>', methods=['PUT'])
 def atualizar_jogo(id): 
-    id_jogo = 0
     dados = request.get_json()
     
-    existe = executar_query("SELECT id FROM produtos WHERE id = ?", id_jogo, fetch=True)
+    existe = executar_query(
+        "SELECT id FROM jogos WHERE id = ?",
+        id,
+        fetch=True
+    )
+
     if not existe:
         return jsonify({"erro": "Jogo não encontrado"}), 404
 
     executar_query(
-        "UPDATE jogos SET jogo = ?, autor = ?, ano = ? , preco = ? WHERE id = ?",
-        (
+        "UPDATE jogos SET jogo = ?, autor = ?, ano = ?, preco = ? WHERE id = ?",
             dados.get('jogo'), 
             dados.get('autor'), 
             dados.get('ano'),
             dados.get('preco'), 
-            id_jogo,
-        ), commit=True
+            id,
+            commit=True
     )
-    return jsonify({"mensagem": "Jogo atualizado com sucesso!"}), 204
+
+    return jsonify({"mensagem": "Jogo atualizado com sucesso!"}), 200
 
 """ DELETE (Remover): Excluir um registro do banco via ID. """
 
-@app.route('/deletarjogo', methods=['DELETE'])
+@app.route('/jogos/<int:id>', methods=['DELETE'])
 
-def deletar_jogo():
+def deletar_jogo(id):
     jogo = executar_query("SELECT jogo FROM jogos WHERE id = ?", id, fetch=True)
     
     if not jogo:
